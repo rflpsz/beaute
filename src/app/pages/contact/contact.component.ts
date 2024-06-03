c7712df089ec23319dad103949e0e9f036fe0f9f
Rafael Souza <rflpsz@gmail.com>
2024-06-03 04:34:59 -0300

    brides and contact fixes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialsComponent } from '../../components/socials/socials.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [SocialsComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
