fbcbfdd031ab92a1aba81847b2c20b6fd59e2451
Rafael Souza <rflpsz@gmail.com>
2024-06-03 04:25:42 -0300

    fixed debutantes page

--- src/app/pages/debutantes/debutantes.component.ts
+++ src/app/pages/debutantes/debutantes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialsComponent } from '../../components/socials/socials.component';
 
 @Component({
   selector: 'app-debutantes',
   standalone: true,
-  imports: [],
+  imports: [SocialsComponent],
   templateUrl: './debutantes.component.html',
   styleUrl: './debutantes.component.scss'
 })
